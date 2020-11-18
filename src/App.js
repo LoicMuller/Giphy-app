ffa081785cd0e3bc4fbc3f1be8a8d9f88f857a1f
Loic Muller <loicdev069@gmail.com>
2020-11-18 16:13:55 +0100

    Landing page updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-function App() {
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./components/Landing";
+import Home from "./components/Home";
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 2000);
+  }, []);
+
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {loading ? <Home /> : <Landing />}
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import logo from "../images/logo.svg";
+
+const Landing = () => {
+  return (
+    <div className="__landing">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+export default Landing;
