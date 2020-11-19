d9f334fff739d4c8eb4edb9acfe6b4b52de96f6a
Loic Muller <loicdev069@gmail.com>
2020-11-19 17:15:03 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+// IMPORT DEPENDENCIES
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// IMPORT COMPONENTS
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
 import LoggedInHeader from "./components/LoggedInHeader";
-import { useState } from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import PasswordReset from "./components/PasswordReset";
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -14,13 +20,18 @@ const App = () => {
       ) : (
         <Header setLoggedIn={setLoggedIn} />
       )}
-      <Switch>
-        {loggedIn ? (
-          <Route exact path="/" component={Welcome} />
-        ) : (
-          <Route exact path="/" component={Home} />
-        )}
-      </Switch>
+      <div className="app__container">
+        <Switch>
+          {loggedIn ? (
+            <Route exact path="/" component={Welcome} />
+          ) : (
+            <Route exact path="/" component={Home} />
+          )}
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Register} />
+          <Route path="/password_reset" component={PasswordReset} />
+        </Switch>
+      </div>
     </Router>
   );
 };
