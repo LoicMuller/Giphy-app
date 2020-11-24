de7ec9fbc93058506550f0936dc2cae781392ba8
Loic Muller <loicdev069@gmail.com>
2020-11-24 18:28:43 +0100

    Modal

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Home = () => {
+  const [modal, setModal] = useState(false);
   const [apiTrendData, setApiTrendData] = useState([]);
 
   const API_KEY = "lYY4eyVwJ7nx1aawRTIfSw1HLV1H0jQP";
@@ -13,7 +14,6 @@ const Home = () => {
       .get(TRENDING_API_URL)
       .then((res) => {
         const gifsItemsArray = res.data.data;
-        console.log(gifsItemsArray);
         setApiTrendData(gifsItemsArray);
       })
       .catch((err) => {
@@ -21,24 +21,36 @@ const Home = () => {
       });
   }, [TRENDING_API_URL]);
 
-  return (
-    <div className="__container ">
-      <h1>Trending</h1>
-      <hr />
-      <div className="wrapper">
-        {apiTrendData.map((data) => (
-          <div className="box" key={data.id}>
-            <a href={data.url} target="_blank" rel="noreferrer">
-              <img src={data.images.downsized.url} alt={data.title} />
-            </a>
-            <div className="__icons">
-              <i className="fas fa-heart"></i>
-              <i className="fas fa-link"></i>
-            </div>
-          </div>
-        ))}
+  const displayModal = modal ? (
+    <div className="modalContainer">
+      <div className="modal abs__center">
+        <h1>Modal</h1>
+        <div onClick={() => setModal(false)}>&times;</div>
+        <button>Log In</button>
       </div>
     </div>
+  ) : null;
+
+  return (
+    <>
+      <div className="__container">
+        <h1>Trending</h1>
+        <hr />
+        <div className="wrapper">
+          {apiTrendData.map((data) => (
+            <div className="box" key={data.id}>
+              <a href={data.url} target="_blank" rel="noreferrer">
+                <img src={data.images.downsized.url} alt={data.title} />
+              </a>
+              <div className="__icons">
+                <i onClick={() => setModal(true)} className="fas fa-heart"></i>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {displayModal}
+    </>
   );
 };
 
