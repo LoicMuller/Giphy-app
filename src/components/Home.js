5cf1647288a267b164e8efb167d52fc1ff8302a4
Loic Muller <loicdev069@gmail.com>
2020-11-25 14:49:09 +0100

    modal updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
@@ -21,12 +22,32 @@ const Home = () => {
       });
   }, [TRENDING_API_URL]);
 
+  const openModal = () => {
+    setModal(true);
+    window.scrollTo(0, 0);
+    document.body.classList.add("stop-scrolling");
+  };
+
+  const closeModal = () => {
+    setModal(false);
+    document.body.classList.remove("stop-scrolling");
+  };
+
   const displayModal = modal ? (
     <div className="modalContainer">
-      <div className="modal abs__center">
-        <h1>Modal</h1>
-        <div onClick={() => setModal(false)}>&times;</div>
-        <button>Log In</button>
+      <div className="modal abs__center flex fd_col align_itm_ctr">
+        <h1>You must login or sign up to save GIFs in your collection</h1>
+        <div className="__close-btn" onClick={closeModal}>
+          &times;
+        </div>
+        <div className="__btn-grp">
+          <button className="btn">
+            <Link to="/login">Log In</Link>
+          </button>
+          <button className="btn">
+            <Link to="/signup">Sign Up</Link>
+          </button>
+        </div>
       </div>
     </div>
   ) : null;
@@ -43,7 +64,7 @@ const Home = () => {
                 <img src={data.images.downsized.url} alt={data.title} />
               </a>
               <div className="__icons">
-                <i onClick={() => setModal(true)} className="fas fa-heart"></i>
+                <i onClick={openModal} className="fas fa-heart"></i>
               </div>
             </div>
           ))}
