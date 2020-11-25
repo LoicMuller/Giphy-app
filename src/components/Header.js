104a3fc8ed18fb44bb564ebed1272e1dc34db3a7
Loic Muller <loicdev069@gmail.com>
2020-11-25 16:45:24 +0100

    dynamic routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
 import { Link } from "react-router-dom";
 import logoHeader from "../images/logo.png";
 import { useHistory } from "react-router-dom";
+import { auth } from "../Firebase/firebase";
 
-const Header = ({ setLoggedIn }) => {
+const Header = ({ loggedIn, setLoggedIn }) => {
   const history = useHistory();
+
+  const signOut = () => {
+    auth.signOut();
+    setLoggedIn(false);
+    history.push("/");
+  };
+
+  const signInBtn = !loggedIn && (
+    <Link title="Click here to Sign In" to="/login">
+      Sign In
+    </Link>
+  );
+
+  const signUpBtn = !loggedIn && (
+    <Link title="Click here to Sign Up" to="/signup">
+      Sign Up
+    </Link>
+  );
+
+  const signOutBtn = loggedIn && (
+    <Link title="Click here to Sign Out" to="/" onClick={signOut}>
+      Sign Out
+    </Link>
+  );
+
   return (
     <nav className="__header flex dark__bg">
       <div className="flex align_itm_ctr">
@@ -16,16 +42,9 @@ const Header = ({ setLoggedIn }) => {
         />
       </div>
       <div className="flex align_itm_ctr">
-        <Link
-          title="Click here to Sign In"
-          to="/login"
-          onClick={() => setLoggedIn(true)}
-        >
-          Sign In
-        </Link>
-        <Link title="Click here to Sign Up" to="/signup">
-          Sign Up
-        </Link>
+        {signInBtn}
+        {signUpBtn}
+        {signOutBtn}
       </div>
     </nav>
   );
